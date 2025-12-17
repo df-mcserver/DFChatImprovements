c4f69554d835b5f096f031bfb54511474ac58218
deadfry42 <pooface442@gmail.com>
2025-12-17 19:55:05 +0000

    move emoji mappings to a seperate file

--- src/main/java/uk/co/nikodem/dFChatImprovements/DFChatImprovements.java
+++ src/main/java/uk/co/nikodem/dFChatImprovements/DFChatImprovements.java
@@ -38,49 +38,9 @@ public final class DFChatImprovements extends JavaPlugin implements Listener {
             } else if (word.startsWith(":") && word.endsWith(":")) {
                 // emoji
                 String emojiName = word.substring(1, word.length()-1).toLowerCase();
-                switch (emojiName) {
-                    case "skull" -> msg = msg.replace(word, "\uE900");
-                    case "cold_sunglasses" -> msg = msg.replace(word, "\uE901");
-                    case "sob" -> msg = msg.replace(word, "\uE902");
-                    case "chair" -> msg = msg.replace(word, "\uE903");
-                    case "sunglasses" -> msg = msg.replace(word, "\uE904");
-                    case "smile" -> msg = msg.replace(word, "\uE905");
-                    case "money_mouth", "money_mouth_face" -> msg = msg.replace(word, "\uE906");
-                    case "pray", "folded_hands" -> msg = msg.replace(word, "\uE907");
-                    case "smiling_face_with_3_hearts", "3_hearts" -> msg = msg.replace(word, "\uE908");
-                    case "smirk_cat" -> msg = msg.replace(word, "\uE909");
-                    case "cold_face", "cold" -> msg = msg.replace(word, "\uE90A");
-                    case "speaking_head", "speaking_head_in_silhouette" -> msg = msg.replace(word, "\uE90B");
-                    case "fire", "flame" -> msg = msg.replace(word, "\uE90C");
-                    case "fish" -> msg = msg.replace(word, "\uE90D");
-                    case "moai", "moyai" -> msg = msg.replace(word, "\uE90E");
-                    case "thumbs_up" -> msg = msg.replace(word, "\uE90F");
-                    case "salute" -> msg = msg.replace(word, "\uE910");
-                    case "shrug" -> msg = msg.replace(word, "\uE911");
-                    case "wompwomp" -> msg = msg.replace(word, "\uE912");
-                    case "joobi", "cold_cry" -> msg = msg.replace(word, "\uE913");
-                    case "wave", "waving_hand" -> msg = msg.replace(word, "\uE914");
-                    case "pensive", "pensive_face" -> msg = msg.replace(word, "\uE915");
-                    case "scream" -> msg = msg.replace(word, "\uE916");
-                    case "smiling_imp" -> msg = msg.replace(word, "\uE917");
-                    case "neutral_face", "neutral" -> msg = msg.replace(word, "\uE918");
-                    case "slight_smile", "slightly_smiling_face" -> msg = msg.replace(word, "\uE919");
-                    case "flushed" -> msg = msg.replace(word, "\uE91A");
-                    case "astonished" -> msg = msg.replace(word, "\uE91B");
-                    case "face_vomiting", "vomiting" -> msg = msg.replace(word, "\uE91C");
-                    case "scream_cat" -> msg = msg.replace(word, "\uE91D");
-                    case "pleading", "pleading_face" -> msg = msg.replace(word, "\uE91E");
-                    case "angry", "angry_face" -> msg = msg.replace(word, "\uE91F");
-                    case "rage", "rage_face", "pouting_face" -> msg = msg.replace(word, "\uE920");
-                    case "nerd", "nerd_face" -> msg = msg.replace(word, "\uE921");
-                    case "beginner", "japanese_symbol_for_beginner" -> msg = msg.replace(word, "\uE922");
 
-                    default -> {
-
-                    }
-                }
-
-
+                String glyphChar = EmojiMappings.mappings.get(emojiName);
+                if (glyphChar != null) msg = msg.replace(word, glyphChar);
             }
         }
 
---
+++ src/main/java/uk/co/nikodem/dFChatImprovements/EmojiMappings.java
@@ -0,0 +1,86 @@
+package uk.co.nikodem.dFChatImprovements;
+
+import java.util.Map;
+
+public class EmojiMappings {
+    public static Map<String, String> mappings = Map.<String, String>ofEntries(
+            Map.entry("skull", "\uE900"),
+            Map.entry("cold_sunglasses", "\uE901"),
+            Map.entry("sob", "\uE902"),
+            Map.entry("chair", "\uE903"),
+            Map.entry("sunglasses", "\uE904"),
+            Map.entry("smile", "\uE905"),
+
+            Map.entry("money_mouth", "\uE906"),
+            Map.entry("money_mouth_face", "\uE906"),
+
+            Map.entry("pray", "\uE907"),
+            Map.entry("folded_hands", "\uE907"),
+
+            Map.entry("3_hearts", "\uE908"),
+            Map.entry("smiling_face_with_3_hearts", "\uE908"),
+
+            Map.entry("smirk_cat", "\uE909"),
+
+            Map.entry("cold", "\uE90A"),
+            Map.entry("cold_face", "\uE90A"),
+
+            Map.entry("speaking_head", "\uE90B"),
+            Map.entry("speaking_head_in_silhouette", "\uE90B"),
+
+            Map.entry("fire", "\uE90C"),
+            Map.entry("flame", "\uE90C"),
+
+            Map.entry("fish", "\uE90D"),
+
+            Map.entry("moai", "\uE90E"),
+            Map.entry("moyai", "\uE90E"),
+
+            Map.entry("thumbs_up", "\uE90F"),
+            Map.entry("salute", "\uE910"),
+            Map.entry("shrug", "\uE911"),
+            Map.entry("wompwomp", "\uE912"),
+
+            Map.entry("joobi", "\uE913"),
+            Map.entry("cold_cry", "\uE913"),
+
+            Map.entry("waving_hand", "\uE914"),
+            Map.entry("wave", "\uE914"),
+
+            Map.entry("pensive", "\uE915"),
+            Map.entry("pensive_face", "\uE915"),
+
+            Map.entry("scream", "\uE916"),
+            Map.entry("smiling_imp", "\uE917"),
+
+            Map.entry("neutral", "\uE918"),
+            Map.entry("neutral_face", "\uE918"),
+
+            Map.entry("slight_smile",  "\uE919"),
+            Map.entry("slightly_smiling_face", "\uE919"),
+
+            Map.entry("flushed", "\uE91A"),
+            Map.entry("astonished", "\uE91B"),
+
+            Map.entry("vomiting", "\uE91C"),
+            Map.entry("face_vomiting", "\uE91C"),
+
+            Map.entry("scream_cat", "\uE91D"),
+
+            Map.entry("pleading", "\uE91E"),
+            Map.entry("pleading_face", "\uE91E"),
+
+            Map.entry("angry", "\uE91F"),
+            Map.entry("angry_face", "\uE91F"),
+
+            Map.entry("rage", "\uE920"),
+            Map.entry("rage_face", "\uE920"),
+            Map.entry("pouting_face", "\uE920"),
+
+            Map.entry("nerd", "\uE921"),
+            Map.entry("nerd_face", "\uE921"),
+
+            Map.entry("beginner", "\uE922"),
+            Map.entry("japanese_symbol_for_beginner", "\uE922")
+    );
+}
