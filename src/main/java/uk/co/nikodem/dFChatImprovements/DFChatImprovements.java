41d9f3b929df1ecd645937c36c435c9ce08a9ed0
deadfry42 <pooface442@gmail.com>
2025-12-17 20:06:06 +0000

    create basic proxy abstraction

--- src/main/java/uk/co/nikodem/dFChatImprovements/DFChatImprovements.java
+++ src/main/java/uk/co/nikodem/dFChatImprovements/DFChatImprovements.java
@@ -5,9 +5,13 @@ import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import uk.co.nikodem.dFChatImprovements.PluginMessaging.ProxyAbstractions;
 
 public final class DFChatImprovements extends JavaPlugin implements Listener {
 
@@ -16,7 +20,13 @@ public final class DFChatImprovements extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void OnDeath(PlayerDeathEvent e) {
+        Player plr = e.getEntity();
+        ProxyAbstractions.sendDeathMessage(plr, e.getDeathMessage());
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
     public void OnChat(AsyncPlayerChatEvent e) {
         // chat modifications
 
@@ -46,6 +56,8 @@ public final class DFChatImprovements extends JavaPlugin implements Listener {
 
         // make \: replace with : so you can say things like :skull: without it turning into an emoji
         msg = msg.replace("\\:", ":");
+        // make \/ replace with / so you can type out commands without running them
+        msg = msg.replace("\\/", "/");
 
         e.setMessage(msg);
     }
---
+++ src/main/java/uk/co/nikodem/dFChatImprovements/PluginMessaging/ProxyAbstractions.java
@@ -0,0 +1,25 @@
+package uk.co.nikodem.dFChatImprovements.PluginMessaging;
+
+import org.bukkit.entity.Player;
+
+public class ProxyAbstractions {
+    public static void sendDeathMessage(Player plr, String deathMessage) {
+
+    }
+
+    public static void sendAdvancementMessage(Player plr, String advancementMessage) {
+
+    }
+
+    public static void sendPlayerMessage(Player plr, String message) {
+
+    }
+
+    public static void setupChannels(Player plr) {
+
+    }
+
+    public static void requestBridgeAccess(Player plr) {
+
+    }
+}
